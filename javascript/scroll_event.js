d842375b9cafbb015044b68a2f1b0fa075af2033
hwabin <agano92@naver.com>
2020-01-12 01:02:06 +0900

    #2 이화빈 / 20/ 1/ 12/ 01:01
    이름수정

--- javascript/scroll_event.js
+++ javascript/scroll_event.js
@@ -1,11 +1,11 @@
-//hwabin 테스트. 
+//이화빈 테스트. 
 
 var setInterval_scroll = setInterval(scroll_loc,10);
 var scroll_y = window.scrollY;
 function scroll_loc(){
 	scroll_y = window.scrollY;
 	if(scroll_y >= 5000){
-		alert('this is scroll_event testing from : hwabin');
+		alert('this is scroll_event testing from : 이화빈');
 	}
 	if(scroll_y >= 5000){
 		clearInterval(setInterval_scroll);
