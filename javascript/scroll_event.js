1d3c867b00d21f4835c67e3d6074bd43f6089148
hwabin <agano92@naver.com>
2020-01-12 00:57:39 +0900

    #1 hwabin / 20/1/12/ 00:57

---
+++ javascript/scroll_event.js
@@ -0,0 +1,14 @@
+//hwabin 테스트. 
+
+var setInterval_scroll = setInterval(scroll_loc,10);
+var scroll_y = window.scrollY;
+function scroll_loc(){
+	scroll_y = window.scrollY;
+	if(scroll_y >= 5000){
+		alert('this is scroll_event testing from : hwabin');
+	}
+	if(scroll_y >= 5000){
+		clearInterval(setInterval_scroll);
+	}
+}
+
